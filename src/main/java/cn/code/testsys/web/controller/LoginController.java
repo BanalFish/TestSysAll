a5894e9747ac963ebcde9fa9a561254a95253a2b
BanalFish <2310092445@qq.com>
2022-02-12 00:26:58 +0800

    拦截登录好了（应该）

--- src/main/java/cn/code/testsys/web/controller/LoginController.java
+++ src/main/java/cn/code/testsys/web/controller/LoginController.java
@@ -3,6 +3,10 @@ package cn.code.testsys.web.controller;
 import cn.code.testsys.domain.User;
 import cn.code.testsys.qo.JSONResult;
 import cn.code.testsys.service.IUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +20,26 @@ public class LoginController {
 
     @RequestMapping("/userLogin")
     @ResponseBody
-    public JSONResult userLogin(String username,String password){
-        User user = userService.getNameAndPass(username, password);
-        if(user != null){
-            return new JSONResult(true ,"登陆成功",user);
-        }else{
-            return new JSONResult(false,"请重新登录");
+//    public JSONResult userLogin(String username,String password){
+//        User user = userService.getNameAndPass(username, password);
+//        if(user != null){
+//            return new JSONResult(true ,"登陆成功");
+//        }else{
+//            return new JSONResult(false,"请重新登录");
+//        }
+//    }
+
+    public JSONResult userlogin(String username, String password){
+        try {
+            UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, password);
+            Subject subject = SecurityUtils.getSubject();
+            subject.login(usernamePasswordToken);
+        }catch (AuthenticationException e){
+            e.printStackTrace();
+            return new JSONResult(false,"fail");
         }
+        return new JSONResult(true,"success");
+
     }
+
 }
